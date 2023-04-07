0eef3c64f4d7b23cb6e1d8b6be7fd3a7f8b46772
Filip Rak <filip.rak@ikol.com>
2023-04-08 01:19:49 +0200

    Cleanup

--- dist/src/utils/helpers.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function clamp(number: number, min: number, max: number): number;
-export declare function formatCssValue(value: number, units: String): string;
-//# sourceMappingURL=helpers.d.ts.map
