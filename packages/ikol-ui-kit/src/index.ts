8cb06f6682a8c58888259531f16fbaabc21ceaf7
Filip Rak <filip.rak@ikol.com>
2023-04-07 23:39:49 +0200

    Cleanup

---
+++ packages/ikol-ui-kit/src/index.ts
@@ -0,0 +1,2 @@
+import '@/components/IkIcon';
+import '@/components/IkLoaderCircle';
---
+++ packages/ikol-ui-kit/vite.config.js
@@ -0,0 +1,32 @@
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+
+export default {
+  build: {
+    minify: false,
+    target: 'esnext',
+    lib: {
+      name: 'abcde',
+      entry: './src/index.ts',
+      formats: ['es'],
+    },
+  },
+  rollupOptions: {
+    input: {
+      IkLoaderCircle: './src/components/IkLoaderCircle/index.ts',
+      IkIcon: './src/components/IkIcon/index.ts',
+    },
+    external: ['vue'],
+    output: {
+      preserveModules: true,
+    },
+  },
+  plugins: [
+    vue()
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+}
