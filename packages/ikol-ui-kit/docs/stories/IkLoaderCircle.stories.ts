fec7bc0f1c9edf71e368c495646a793478c3d19b
Filip Rak <filip.rak@ikol.com>
2023-04-18 23:02:36 +0200

    Cleanup

--- packages/ikol-ui-kit/docs/stories/IkLoaderCircle.stories.ts
+++ packages/ikol-ui-kit/docs/stories/IkLoaderCircle.stories.ts
@@ -2,6 +2,7 @@ import type { Meta } from '@storybook/vue3';
 import { IkLoaderCircle } from '@/components/IkLoaderCircle';
 import { h } from 'vue';
 import { useTheme } from '@/composables';
+import { UIDesignColor } from '@/types/utils';
 
 
 const meta: Meta<typeof IkLoaderCircle> = {
@@ -22,15 +23,39 @@ const meta: Meta<typeof IkLoaderCircle> = {
           const { dark_theme, ...props } = args;
           theme.is_dark.value = dark_theme;
 
-          return h(IkLoaderCircle, props);
+          return Object
+            .values(UIDesignColor)
+            .map(design => {
+              return h(IkLoaderCircle, { class: 'ik-mr-3', design, ...props })
+            });
         };
       },
     };
   }
 };
 
-export const Preview = {
-  args: {},
+export const Default = {
+  args: { value: 66 },
+};
+
+export const Indeterminate = {
+  args: { indeterminate: true },
+};
+
+export const Progress = {
+  args: { value: 30 },
+};
+
+export const Rotate = {
+  args: { rotate: 90, value: 30 },
+};
+
+export const Thicnkess = {
+  args: { thickness: 6, value: 30 },
+};
+
+export const Size = {
+  args: { size: 40, value: 30 },
 };
 
 export default meta;
