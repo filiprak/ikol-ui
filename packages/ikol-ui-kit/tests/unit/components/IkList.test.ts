3505f4255c817e76ce67689bfaf7a6bcf802170f
Filip Rak <filip.rak@ikol.com>
2023-05-07 19:58:21 +0200

    Cleanup

---
+++ packages/ikol-ui-kit/tests/unit/components/IkList.test.ts
@@ -0,0 +1,23 @@
+import { renderShallow } from '@/tests/tools/utils';
+import { IkList } from '@/components/IkList';
+
+describe('IkList', () => {
+    const getInstance = (options = {}) => renderShallow(IkList, options);
+
+    it('renders with correct default state', () => {
+        const wrapper = getInstance();
+        expect(wrapper.classes()).toStrictEqual([
+            'ik-list',
+        ]);
+        expect(wrapper.element.tagName).toBe('DIV');
+    });
+
+    it('renders with default slot', () => {
+        const wrapper = getInstance({
+            slots: {
+                default: '<div class="test">List content slot.</div>',
+            },
+        });
+        expect(wrapper.text()).toContain('List content slot.');
+    });
+});
