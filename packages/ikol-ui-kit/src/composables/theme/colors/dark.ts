8159a0bad2d9714b39ec00398e9f46b515cbfa97
Filip Rak <filip.rak@ikol.com>
2023-05-07 20:56:16 +0200

    Cleanup

---
+++ packages/ikol-ui-kit/src/composables/theme/colors/dark.ts
@@ -0,0 +1,86 @@
+import { ThemeColors } from "@/composables/theme";
+
+export default new ThemeColors({
+    background_color: '#121212',
+    on_background_color_1: '#cdc3ba',
+    on_background_color_2: '#b6aea4',
+    on_background_color_3: '#787878',
+    on_background_color_4: '#4b4b4b',
+    on_background_color_5: '#2c2c2c',
+
+    surface_color: '#222222',
+    on_surface_color_1: '#cdc3ba',
+    on_surface_color_2: '#b6aea4',
+    on_surface_color_3: '#787878',
+    on_surface_color_4: '#4b4b4b',
+
+    surface_lighten_color: '#191919',
+    on_surface_lighten_color_1: '#e0d3c7',
+    on_surface_lighten_color_2: '#93908b',
+    on_surface_lighten_color_3: '#7a736e',
+    on_surface_lighten_color_4: '#696969',
+
+    surface_darken_color: '#222222',
+    on_surface_darken_color_1: '#e0d3c7',
+    on_surface_darken_color_2: '#93908b',
+    on_surface_darken_color_3: '#7a736e',
+    on_surface_darken_color_4: '#696969',
+
+    border_color_1: '#474747',
+    border_color_2: '#363636',
+    marker_color_1: '#737373',
+    skeleton_color_1: '#333333',
+    skeleton_color_2: 'hsla(0, 0%, 100%, .03)',
+
+    primary_color: '#5989ff',
+    primary_color_o1: 'rgba(100, 136, 248, 0.1)',
+    on_primary_color: '#fff',
+
+    secondary_color: '#1b2c39',
+    secondary_color_alt_1: '#0D3034',
+    on_secondary_color: '#fff',
+    on_secondary_color_alt_1: '#fff',
+
+    accent_color: '#F3B563',
+    accent_color_o1: 'rgba(243, 181, 99, 0.1)',
+    on_accent_color: '#1f2c38',
+
+    error_color: '#FF304C',
+    error_color_o1: 'rgba(255, 48, 76, 0.1)',
+    on_error_color: '#fff',
+
+    success_color: '#40BE7A',
+    success_color_o1: 'rgba(64, 190, 122, 0.1)',
+    on_success_color: '#fff',
+
+    shadow_color_1: 'rgba(0, 0, 0, 0.5)',
+    shadow_color_2: 'rgba(0, 0, 0, 1)',
+    shadow_color_3: 'rgba(0, 0, 0, 1)',
+
+    scrollbar_color: '#fff',
+    scrollbar_track_color: 'rgba(255, 255, 255, 0.1)',
+
+    tooltip_color: '#444',
+    on_tooltip_color: '#e0d3c7',
+
+    theme_1_color: '#6488F8',
+    theme_1_color_o1: 'rgba(100, 136, 248, 0.1)',
+    theme_2_color: '#1FB665',
+    theme_2_color_o1: 'rgba(31, 182, 101, 0.1)',
+    theme_3_color: '#F4C23B',
+    theme_3_color_o1: 'rgba(244, 194, 59, 0.1)',
+    theme_4_color: '#FF855D',
+    theme_4_color_o1: 'rgba(255, 133, 93, 0.1)',
+    theme_5_color: '#FF304C',
+    theme_5_color_o1: 'rgba(255, 48, 76, 0.1)',
+    theme_6_color: '#FF179E',
+    theme_6_color_o1: 'rgba(255, 23, 158, 0.1)',
+    theme_7_color: '#A036CC',
+    theme_7_color_o1: 'rgba(160, 54, 204, 0.1)',
+    theme_8_color: '#212121',
+    theme_8_color_o1: 'rgba(33, 33, 33, 0.1)',
+    theme_9_color: '#3AAADC',
+    theme_9_color_o1: 'rgba(58, 170, 220, 0.1)',
+
+    on_theme_color: '#fff',
+});
---
+++ packages/ikol-ui-kit/src/composables/theme/index.ts
@@ -0,0 +1,191 @@
+import type { InjectionKey, Ref } from 'vue';
+import LightColors from '@/styles/themes/light';
+import DarkColors from '@/styles/themes/dark';
+import { getCurrentInstance, inject, provide, watchEffect, watch, ref } from 'vue';
+
+export enum ThemeType {
+    DARK = 'dark',
+    LIGHT = 'light',
+}
+
+const COLORS_MAP = {
+    'light': LightColors,
+    'dark': DarkColors,
+};
+
+export type ThemeOptions = {
+    type?: ThemeType,
+    variant?: number | null,
+}
+
+export class ThemeColors {
+    readonly background_color: string | null = null;
+    readonly on_background_color_1: string | null = null;
+    readonly on_background_color_2: string | null = null;
+    readonly on_background_color_3: string | null = null;
+    readonly on_background_color_4: string | null = null;
+    readonly on_background_color_5: string | null = null;
+
+    readonly surface_color: string | null = null;
+    readonly on_surface_color_1: string | null = null;
+    readonly on_surface_color_2: string | null = null;
+    readonly on_surface_color_3: string | null = null;
+    readonly on_surface_color_4: string | null = null;
+
+    readonly surface_darken_color: string | null = null;
+    readonly on_surface_darken_color_1: string | null = null;
+    readonly on_surface_darken_color_2: string | null = null;
+    readonly on_surface_darken_color_3: string | null = null;
+    readonly on_surface_darken_color_4: string | null = null;
+
+    readonly surface_lighten_color: string | null = null;
+    readonly on_surface_lighten_color_1: string | null = null;
+    readonly on_surface_lighten_color_2: string | null = null;
+    readonly on_surface_lighten_color_3: string | null = null;
+    readonly on_surface_lighten_color_4: string | null = null;
+
+    readonly border_color_1: string | null = null;
+    readonly border_color_2: string | null = null;
+    readonly marker_color_1: string | null = null;
+    readonly skeleton_color_1: string | null = null;
+    readonly skeleton_color_2: string | null = null;
+
+    readonly primary_color: string | null = null;
+    readonly primary_color_o1: string | null = null;
+    readonly on_primary_color: string | null = null;
+
+    readonly secondary_color: string | null = null;
+    readonly secondary_color_alt_1: string | null = null;
+    readonly on_secondary_color: string | null = null;
+    readonly on_secondary_color_alt_1: string | null = null;
+
+    readonly accent_color: string | null = null;
+    readonly accent_color_o1: string | null = null;
+    readonly on_accent_color: string | null = null;
+
+    readonly error_color: string | null = null;
+    readonly error_color_o1: string | null = null;
+    readonly on_error_color: string | null = null;
+
+    readonly success_color: string | null = null;
+    readonly success_color_o1: string | null = null;
+    readonly on_success_color: string | null = null;
+
+    readonly shadow_color_1: string | null = null;
+    readonly shadow_color_2: string | null = null;
+    readonly shadow_color_3: string | null = null;
+
+    readonly scrollbar_color: string | null = null;
+    readonly scrollbar_track_color: string | null = null;
+
+    readonly tooltip_color: string | null = null;
+    readonly on_tooltip_color: string | null = null;
+
+    readonly theme_1_color: string | null = null;
+    readonly theme_1_color_o1: string | null = null;
+    readonly theme_2_color: string | null = null;
+    readonly theme_2_color_o1: string | null = null;
+    readonly theme_3_color: string | null = null;
+    readonly theme_3_color_o1: string | null = null;
+    readonly theme_4_color: string | null = null;
+    readonly theme_4_color_o1: string | null = null;
+    readonly theme_5_color: string | null = null;
+    readonly theme_5_color_o1: string | null = null;
+    readonly theme_6_color: string | null = null;
+    readonly theme_6_color_o1: string | null = null;
+    readonly theme_7_color: string | null = null;
+    readonly theme_7_color_o1: string | null = null;
+    readonly theme_8_color: string | null = null;
+    readonly theme_8_color_o1: string | null = null;
+    readonly theme_9_color: string | null = null;
+    readonly theme_9_color_o1: string | null = null;
+
+    readonly on_theme_color: string | null = null;
+
+    constructor(colors: ThemeColors) { Object.assign(this, colors); }
+}
+
+export interface ThemeInstance {
+    readonly type: Ref<ThemeType>,
+    readonly css_classes: Ref<{ [key: string]: boolean }>,
+    is_dark: Ref<boolean>,
+    variant: Ref<number | null>,
+    colors: Ref<ThemeColors>,
+}
+
+export const THEME_SYMBOL: InjectionKey<ThemeInstance> = Symbol.for('ik-theme');
+
+export function provideTheme(options: ThemeOptions) {
+    const theme = createTheme(options);
+
+    provide(THEME_SYMBOL, theme);
+
+    return theme;
+}
+
+function createTheme(options?: ThemeOptions): ThemeInstance {
+    const type = ref(options?.type || ThemeType.LIGHT);
+    const variant = ref<number | null>(options?.variant || null);
+    const is_dark = ref(type.value === ThemeType.DARK);
+    const css_classes = ref<{ [key: string]: boolean }>({});
+    const colors = ref<ThemeColors>(COLORS_MAP['light']);
+
+    function updateType(new_type?: ThemeType) {
+        new_type = new_type || type.value;
+
+        const classes = {
+            'ik-theme': true,
+            [['ik-theme--', new_type].join('')]: true,
+        };
+
+        if (variant.value) {
+            classes[['ik-theme--', variant.value].join('')] = true;
+        }
+
+        css_classes.value = classes;
+        type.value = new_type;
+        colors.value = COLORS_MAP[new_type] || COLORS_MAP['light'];
+    }
+
+    watch(is_dark, (dark) => {
+        updateType(dark ? ThemeType.DARK : ThemeType.LIGHT);
+    });
+    watch(variant, () => {
+        updateType();
+    });
+    updateType(type.value);
+
+    return {
+        type,
+        is_dark,
+        variant,
+        colors,
+        css_classes,
+    };
+}
+
+const global = createTheme();
+
+watchEffect(() => {
+    const el = document.documentElement;
+
+    [...el.classList].forEach(class_name => {
+        if (class_name.startsWith('ik-theme')) {
+            el.classList.remove(class_name);
+        }
+    });
+
+    for (const class_name in global.css_classes.value) {
+        if (global.css_classes.value[class_name]) {
+            el.classList.add(class_name);
+        }
+    }
+});
+
+export function useTheme(): ThemeInstance {
+    if (getCurrentInstance()) {
+        return inject(THEME_SYMBOL, global);
+    } else {
+        return global;
+    }
+}
