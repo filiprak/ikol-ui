3dfc6843eda2296c6d840e99d7d645ffc2a84566
Filip Rak <filip.rak@ikol.com>
2023-05-12 22:43:40 +0200

    Cleanup

--- packages/ikol-ui-kit/docs/stories/IkImage.stories.ts
+++ packages/ikol-ui-kit/docs/stories/IkImage.stories.ts
@@ -12,9 +12,7 @@ const meta: Meta<typeof IkImage> = {
         IkImage,
       },
       template: `
-        <IkImage v-bind="args">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit
-        </IkImage>
+        <IkImage v-bind="args"/>
       `,
       setup() {
         return { args };
@@ -24,7 +22,31 @@ const meta: Meta<typeof IkImage> = {
 };
 
 export const Default: Meta<typeof IkImage> = {
-  args: {},
+  args: { src: 'https://picsum.photos/300/200' },
+};
+
+export const Loader: Meta<typeof IkImage> = {
+  args: { src: 'http://picsum.photos/300/200', loader: true, size: 200 },
+};
+
+export const Size: Meta<typeof IkImage> = {
+  args: { src: 'https://picsum.photos/300/200', size: 150 },
+};
+
+export const WidthAndHeight: Meta<typeof IkImage> = {
+  args: { src: 'https://picsum.photos/300/200', width: 250, height: 150 },
+};
+
+export const Round: Meta<typeof IkImage> = {
+  args: { src: 'https://picsum.photos/200/200', round: true },
+};
+
+export const Contain: Meta<typeof IkImage> = {
+  args: { src: 'http://picsum.photos/300/200', contain: true, size: 300 },
+};
+
+export const Cover: Meta<typeof IkImage> = {
+  args: { src: 'http://picsum.photos/300/200', cover: true, size: 300 },
 };
 
 export default meta;
--- packages/ikol-ui-kit/src/components/IkImage/IkImage.vue
+++ packages/ikol-ui-kit/src/components/IkImage/IkImage.vue
@@ -1,17 +1,104 @@
 <script lang="ts">
 import '@/styles';
 import './IkImage.css';
-import { defineComponent, h } from 'vue';
+import { defineComponent, h, ref, Ref } from 'vue';
+import { IkLoaderCircle } from '@/components/IkLoaderCircle';
 
 export default defineComponent({
     name: 'IkImage',
     props: {
-
+        src: {
+            type: String,
+            default: null
+        },
+        alt: {
+            type: String,
+            default: null
+        },
+        round: {
+            type: Boolean,
+            default: false
+        },
+        size: {
+            type: Number,
+            default: null
+        },
+        width: {
+            type: Number,
+            default: null
+        },
+        height: {
+            type: Number,
+            default: null
+        },
+        width_attr: {
+            type: Number,
+            default: null
+        },
+        height_attr: {
+            type: Number,
+            default: null
+        },
+        contain: {
+            type: Boolean,
+            default: false
+        },
+        cover: {
+            type: Boolean,
+            default: false
+        },
+        loader: {
+            type: Boolean,
+            default: false
+        },
     },
     setup(props, { slots }) {
-        const classes = ['ik-image'];
+        const classes = {
+            'ik-image': true,
+            'ik-image--empty': !props.src,
+            'ik-image--round': props.round,
+            'ik-image--contain': props.contain,
+            'ik-image--cover': props.cover,
+        };
+        const loading = ref(props.loader);
+        const error: Ref<Event | null> = ref(null);
+        const width = props.width || props.size;
+        const height = props.height || props.size;
+        const styles: any = {};
 
-        return () => h('div', { class: classes }, slots.default?.());
+        width && (styles.width = width + 'px');
+        height && (styles.height = height + 'px');
+
+        return () => {
+            const img = h(props.src ? 'img' : 'div', {
+                class: classes,
+                src: props.src,
+                loading: 'lazy',
+                alt: props.alt || '{{_*en*Image_}}',
+                width: props.width_attr || null,
+                height: props.height_attr || null,
+                style: styles,
+
+                onLoad: props.loader ? () => {
+                    loading.value = false;
+                    error.value = null;
+                } : null,
+                onError: props.loader ? (event: Event) => {
+                    loading.value = false;
+                    error.value = event;
+                } : null,
+
+            }, slots.default?.());
+
+            if (props.loader && loading.value) {
+                return h('div', { style: styles, class: ['ik-image', 'ik-image--loading'] }, [
+                    h(IkLoaderCircle, { indeterminate: true }),
+                    img,
+                ]);
+            } else {
+                return img;
+            }
+        };
     },
 });
 </script>
