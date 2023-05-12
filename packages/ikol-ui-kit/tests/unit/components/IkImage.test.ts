e3f46ba9d5a1e93e2f7b3ad11776e9c25978d910
Filip Rak <filip.rak@ikol.com>
2023-05-12 23:05:09 +0200

    Cleanup

--- packages/ikol-ui-kit/tests/unit/components/IkImage.test.ts
+++ packages/ikol-ui-kit/tests/unit/components/IkImage.test.ts
@@ -1,5 +1,6 @@
 import { renderShallow } from '@/tests/tools/utils';
 import { IkImage } from '@/components/IkImage';
+import { IkLoaderCircle } from '@/components/IkLoaderCircle';
 
 describe('IkImage', () => {
     const getInstance = (options = {}) => renderShallow(IkImage, options);
@@ -8,16 +9,161 @@ describe('IkImage', () => {
         const wrapper = getInstance();
         expect(wrapper.classes()).toStrictEqual([
             'ik-image',
+            'ik-image--empty',
         ]);
         expect(wrapper.element.tagName).toBe('DIV');
+        expect(wrapper.element).toMatchSnapshot();
     });
 
-    it('renders with default slot', () => {
+    it('renders with src and alt prop', () => {
         const wrapper = getInstance({
-            slots: {
-                default: '<div class="test">Default slot test.</div>',
+            propsData: {
+                src: 'http://image.com/image.png',
+                alt: 'Image Alt'
+            }
+        });
+        expect(wrapper.element.tagName).toBe('IMG');
+        expect(wrapper.attributes('src')).toBe('http://image.com/image.png');
+        expect(wrapper.attributes('alt')).toBe('Image Alt');
+        expect(wrapper.element).toMatchSnapshot();
+    });
+
+    it('renders rounded', () => {
+        const wrapper = getInstance({
+            propsData: {
+                src: 'http://image.com/image.png',
+                round: true
+            }
+        });
+        expect(wrapper.classes()).toContain('ik-image--round');
+        expect(wrapper.element).toMatchSnapshot();
+    });
+
+    it('renders optimized for window size', () => {
+        const wrapper = getInstance({
+            propsData: {
+                src: 'http://image.com/image.png',
+                contain: true
+            }
+        });
+        expect(wrapper.classes()).toContain('ik-image--contain');
+        expect(wrapper.element).toMatchSnapshot();
+    });
+
+    it('renders with custom size', () => {
+        const wrapper = getInstance({
+            propsData: {
+                src: 'http://image.com/image.png',
+                size: 97
+            }
+        });
+        expect(wrapper.attributes('style')).toContain('width: 97px;');
+        expect(wrapper.attributes('style')).toContain('height: 97px;');
+        expect(wrapper.element).toMatchSnapshot();
+    });
+
+    it('renders with custom width', () => {
+        const wrapper = getInstance({
+            propsData: {
+                src: 'http://image.com/image.png',
+                width: 59
+            }
+        });
+        expect(wrapper.attributes('style')).toContain('width: 59px;');
+        expect(wrapper.attributes('style')).not.toContain('height');
+        expect(wrapper.element).toMatchSnapshot();
+    });
+
+    it('renders with custom height', () => {
+        const wrapper = getInstance({
+            propsData: {
+                src: 'http://image.com/image.png',
+                height: 59
+            }
+        });
+        expect(wrapper.attributes('style')).toContain('height: 59px;');
+        expect(wrapper.attributes('style')).not.toContain('width');
+        expect(wrapper.element).toMatchSnapshot();
+    });
+
+    it('renders with contain param', () => {
+        const wrapper = getInstance({
+            propsData: {
+                src: 'http://image.com/image.png',
+                contain: true
+            }
+        });
+        expect(wrapper.classes()).toContain('ik-image--contain');
+    });
+
+    it('renders with cover param', () => {
+        const wrapper = getInstance({
+            propsData: {
+                src: 'http://image.com/image.png',
+                cover: true
+            }
+        });
+        expect(wrapper.classes()).toContain('ik-image--cover');
+    });
+
+    it('passes click listener', () => {
+        const clickMock = jest.fn();
+
+        const wrapper = getInstance({
+            propsData: {
+                src: 'http://image.com/image.png',
+                onClick: clickMock
             },
         });
-        expect(wrapper.text()).toContain('Default slot test.');
+        wrapper.find('img').trigger('click');
+
+        expect(clickMock).toBeCalled();
+    });
+
+    it('adds custom attribute class', () => {
+        const wrapper = getInstance({
+            propsData: {
+                src: 'http://image.com/image.png',
+            },
+            attrs: { class: 'unit-test ikol' },
+        });
+        wrapper.find('img').trigger('click');
+
+        expect(wrapper.classes()).toContain('unit-test');
+        expect(wrapper.classes()).toContain('ikol');
+    });
+
+    it('works with loader', () => {
+        const wrapper = getInstance({
+            propsData: {
+                src: 'http://image.com/image.png',
+                loader: true,
+            },
+        });
+        expect(wrapper.classes()).toContain('ik-image--loading');
+        expect(wrapper.findComponent(IkLoaderCircle).isVisible()).toBeTruthy();
+        expect(wrapper.find('img').isVisible()).toBeTruthy();
+    });
+
+    it('renders with width attribute', () => {
+        const wrapper = getInstance({
+            propsData: {
+                src: 'http://image.com/image.png',
+                width_attr: 59
+            }
+        });
+        expect(wrapper.attributes('width')).toBe('59');
+        expect(wrapper.element).toMatchSnapshot();
+    });
+
+    it('renders with height attribute', () => {
+        const wrapper = getInstance({
+            propsData: {
+                src: 'http://image.com/image.png',
+                height_attr: 59
+            }
+        });
+        expect(wrapper.attributes('height')).toBe('59');
+        expect(wrapper.element).toMatchSnapshot();
     });
 });
