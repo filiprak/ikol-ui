fc4b508f17a5bcf521e24293f9cf969df538be40
Filip Rak <filip.rak@ikol.com>
2023-05-13 13:06:32 +0200

    Cleanup

--- packages/ikol-ui-kit/src/components/IkDivider/IkDivider.vue
+++ packages/ikol-ui-kit/src/components/IkDivider/IkDivider.vue
@@ -1,17 +1,47 @@
+<template>
+    <div :class="classes">
+        <div v-if="$slots.default"
+             class="ik-divider__content">
+            <slot></slot>
+        </div>
+        <div class="ik-divider__line"></div>
+    </div>
+</template>
+  
 <script lang="ts">
 import '@/styles';
 import './IkDivider.css';
-import { defineComponent, h } from 'vue';
+import { defineComponent } from 'vue'
 
 export default defineComponent({
     name: 'IkDivider',
     props: {
-
+        noBottomMargin: {
+            type: Boolean,
+            default: false
+        },
+        noMargin: {
+            type: Boolean,
+            default: false
+        },
+        onSurface: {
+            type: Boolean,
+            default: false
+        },
     },
-    setup(props, { slots }) {
+    setup(props) {
         const classes = ['ik-divider'];
 
-        return () => h('div', { class: classes }, slots.default?.());
-    },
-});
+        if (props.noMargin) {
+            classes.push('ik-divider--no-margin');
+        } else if (props.noBottomMargin) {
+            classes.push('ik-divider--no-b-margin');
+        }
+        if (props.onSurface) {
+            classes.push('ik-divider--on-surface');
+        }
+
+        return { classes };
+    }
+})
 </script>
