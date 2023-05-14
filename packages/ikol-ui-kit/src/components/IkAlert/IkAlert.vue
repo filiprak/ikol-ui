b0518164eed189cafdee10621ebe0c8cdddc7fab
Filip Rak <filip.rak@ikol.com>
2023-05-14 17:26:19 +0200

    Cleanup [skip ci]

--- packages/ikol-ui-kit/src/components/IkAlert/IkAlert.vue
+++ packages/ikol-ui-kit/src/components/IkAlert/IkAlert.vue
@@ -15,41 +15,40 @@
         </div>
     </div>
 </template>
-<script lang="ts">
+<script setup lang="ts">
 import '@/styles';
 import './IkAlert.css';
 import { IkIcon } from '@/components/IkIcon';
-import { defineComponent } from 'vue';
+import { computed, defineProps } from 'vue';
 
-export default defineComponent({
-    name: 'IkAlert',
-    components: { IkIcon },
-    props: {
-        design: {
-            type: String,
-            default: null,
-        },
-        icon: {
-            type: String,
-            default: null,
-        },
-        variant: {
-            type: String,
-            default: null,
-        },
+const props = defineProps({
+    design: {
+        type: String,
+        default: null,
     },
-    setup(props) {
-        const classes = ['ik-alert'];
-
-        if (props.design) {
-            classes.push(`ik-alert--${props.design}`);
-        }
-
-        if (props.variant) {
-            classes.push(`ik-alert--${props.variant}`);
-        }
-
-        return { classes };
+    icon: {
+        type: String,
+        default: null,
+    },
+    variant: {
+        type: String,
+        default: null,
     },
 });
+const classes = computed(() => {
+    const result = ['ik-alert'];
+
+    if (props.design) {
+        result.push(`ik-alert--${props.design}`);
+    }
+
+    if (props.variant) {
+        result.push(`ik-alert--${props.variant}`);
+    }
+
+    return result;
+});
+</script>
+<script lang="ts">
+export default { name: 'IkAlert' };
 </script>
