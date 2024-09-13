d198f7efc4b1cdb83e5af7a93fc12db291ec87b9
Filip Rak <filip.rak@ikol.com>
2024-09-13 11:44:15 +0200

    Cleanup

--- .release/sync.sh
+++ .release/sync.sh
@@ -5,3 +5,11 @@ tar -cC ~/ikol/ikol-webapp/client/packages/ui-kit/ \
     --exclude='dist' \
     --exclude='coverage' \
     --exclude='lib' . | tar -xC ./packages/ui-kit/
+
+tar -cC ~/ikol/ikol-webapp/client/packages/eslint-config-ts-vue3/ \
+    --exclude='node_modules' \
+    --exclude='package-lock.json' \
+    --exclude='docs/dist' \
+    --exclude='dist' \
+    --exclude='coverage' \
+    --exclude='lib' . | tar -xC ./packages/eslint-config-ts-vue3/
